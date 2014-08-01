e2fa7d2a41c7c0faeee84fa748d41962e845b765
Benjamin R. Haskell <ruby@benizi.com>
2014-07-31 21:12:55 -0400

    Move script out of ARGV
    It's annoying that the whole shell script shows up as an argument to `sh
    -c` in tools like `ps`.  Instead, dump the script into a temp file.
    
        $ ps -faux | awk -F\\ '/mux/&&/_ sh/ {$1="";print $0}'
    
        # before:
        _ sh -c #!/bin/zsh tmux start-server\; has-session [...1475 more chars...]
    
        # after:
        _ sh /tmp/tmuxinator20140731-11503-1bnc7e1

--- lib/tmuxinator/util.rb
+++ lib/tmuxinator/util.rb
@@ -10,5 +10,15 @@ module Tmuxinator
     def yes_no(condition)
       condition ? say("Yes", :green) : say("No", :red)
     end
+
+    def exec_script(script)
+      temp = Tempfile.new('tmuxinator')
+      temp.write(script)
+      temp.close
+      Kernel.system('sh', temp.path)
+      exit $?.exitstatus
+    ensure
+      temp.unlink
+    end
   end
 end
